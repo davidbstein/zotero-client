a2ebb16ded6a3f29846e592a5451471653ad91c6
David Stein <davidbstein@gmail.com>
2022-09-12 18:46:54 -0400

    remove old code and add TODOs

--- src/main/zotero-backend.js
+++ src/main/zotero-backend.js
@@ -3,39 +3,23 @@ import { app, ipcMain } from "electron";
 import fetch from "electron-fetch";
 
 /**
- * 
- *  Header <Last-Modified-Version> will let me not re-load everything every time.
+ *  TODO: caching
+ *  Header <Last-Modified-Version> will let me not re-load everything every time. SHould both read and send it down with every request
  *  Header <If-Modified-Since-Version> will check for updates
  *  (https://www.zotero.org/support/dev/web_api/v3/syncing)
  * 
  * 
- * 
+ * TODO: update and create
+ *  POST will create an object
+ *  PUT will replace all fields
+ *  PATCH will update included fields
+ * (both can take a list of up to 50 objects with keys)
  */
 
 const homeDir = app.getPath("home");
 const secrets = JSON.parse(fs.readFileSync(`${homeDir}/.secrets.json`, "utf8"));
 const USER_ID = secrets.zotero.user_library_id;
 const USER_TOKEN = secrets.zotero.pyzotero_api_key;
-
-async function getCollections({group_id}) {
-  return await doZoteroGetRequest({
-    path: "collections",
-    group_id,
-  });
-}
-
-async function getGroups() {
-  return await doZoteroGetRequest({
-    path: "groups"
-  });
-}
-
-async function getItems({group_id}) {
-  return await doZoteroGetRequest({
-    path: "items",
-    group_id
-  });
-}
 
 async function doZoteroGetRequest({method="GET", group_id, path, params}) {
   const userOrGroupPrefix = group_id ? "group" : "users";
@@ -61,6 +45,7 @@ async function doZoteroGetRequest({method="GET", group_id, path, params}) {
         headers,
       });
       total = resp.headers.get("total-results")
+      const versionNo = resp.headers.get("last-modified-version")
       to_ret.push(...(await resp.json()));
       console.log("to_ret", to_ret, to_ret.length, total);
     }
